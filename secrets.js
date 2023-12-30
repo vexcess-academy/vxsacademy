44988267373be1248a3caa7e92015ff2a59d2f83
vExcess <41875927+vExcess@users.noreply.github.com>
2023-12-30 13:11:36 -0800

    standardize formatting

--- secrets.js
+++ secrets.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
 
 module.exports = {
-	"MASTER_KEY": "1234",
-  	"RECAPTCHA_KEY": "1234",
-	key: fs.readFileSync("../server.key"),
-	cert: fs.readFileSync("../server.cert"),
-	port: 1234,
-	sandboxPort: 1234,
-	databaseURL: "mongodb://localhost:1234"
+	MASTER_KEY: "abc123",
+  	RECAPTCHA_KEY: "abc123",
+	KEY: fs.readFileSync("../server.key"),
+	CERT: fs.readFileSync("../server.cert"),
+	PORT: 3000,
+	SANDBOX_PORT: 3001,
+	MONGO_IP: "localhost",
+	MONGO_PORT: "27017"
 };
+
